5909da685302e1fdf9aa7b747fefe418e073fb9d
Surinderbansal55 <122786499+Surinderbansal55@users.noreply.github.com>
2023-01-25 17:35:35 +0530

    Add files via upload
    Reading text file using file reader and buffer reader

---
+++ buffer Reader.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+public class test {
+    public static void main(String args[])throws Exception{
+        FileReader fr=new FileReader("C:\\Users\\ASUS\\OneDrive\\Documents\\binart_to_octal.txt");
+        BufferedReader br=new BufferedReader(fr);
+
+        int i;
+        while((i=br.read())!=-1){
+            System.out.print((char)i);
+        }
+        br.close();
+        fr.close();
+    }
+}
