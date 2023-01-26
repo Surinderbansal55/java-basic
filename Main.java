ef3c00952603557e1a675c69b4e2b4f2e41c6ca9
Surinderbansal55 <122786499+Surinderbansal55@users.noreply.github.com>
2023-01-26 20:23:38 +0530

    Add files via upload
    Buffer reader input output

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args)throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine());    //reads the input as string
+        int t=Integer.parseInt(br.readLine()); //convert input string to integer
+        System.out.println(t); //print integer output
+    }
+}
